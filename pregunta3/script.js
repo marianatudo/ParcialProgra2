db6076b1c5ae0c545fd962572a9b31fff624881c
Mariana   Tudó Basagoitia <20192111@esen.edu.sv>
2021-02-23 20:39:11 -0600

    init files

---
+++ pregunta3/script.js
@@ -0,0 +1,19 @@
+//Enviar información
+
+function sendInfo() {
+    var table = document.getElementById("table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var date = document.getElementById("date").value;
+    var drink = document.getElementById("drink").value;
+    var glass = document.getElementById("glass").value;
+    cell1.innerHTML = date;
+    cell2.innerHTML = drink;
+    cell3.innerHTML = glass;
+    cell4.innerHTML = "Tomar más agua";
+}
+
+
